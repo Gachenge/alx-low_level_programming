ec71378b2dbe6a06ed766109c70d1d029438bf8b
Gachenge <gachenge1@gmail.com>
2022-09-08 12:16:25 +0000

    Sizeof

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - find sizes of types
+ *
+ * Return: Another nun
+ */
+int main(void)
+{
+	char charSize;
+	int intSize;
+	long longInt;
+	long long longLong;
+	float floatSize;
+printf("Size of a char: %f\n", (sizeof(charSize));
+printf("Size of an int: %f\n", (sizeof(intSize));
+printf("Size of a long int: %f\n", (sizeof(longInt));
+printf("Size of a long long int: %f\n", (sizeof(longLong));
+printf("Size of a float: %f\n", (sizeof(floatSize));
+	return (0);
+}
