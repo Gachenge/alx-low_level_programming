0baaf5636638039d39b1462838ba2162c8546c28
Gachenge <gachenge1@gmail.com>
2022-09-09 13:07:56 +0000

    alpbab

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - start the prog
+ *
+ * Return: end
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	for (a = 'A'; a <= 'Z'; a++)
+	{
+		putchar(a);
+	}
+		return (0);
+}
+
