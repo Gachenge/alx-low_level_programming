cffc533f3ec8b7e727de5bbbc24ec650d4201c39
Gachenge <gachenge1@gmail.com>
2022-09-09 12:46:52 +0000

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - start the prog
+ *
+ * Return: end
+ */
+int main(void)
+{
+char a;
+for (a = 'a'; a <= 'z'; a++)
+{
+	putchar(a);
+}
+putchar('\n');
+return (0);
+}
