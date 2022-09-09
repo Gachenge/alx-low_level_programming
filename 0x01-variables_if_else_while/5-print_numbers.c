29e92c161b65975a9de840256e923dd75201acac
Gachenge <gachenge1@gmail.com>
2022-09-09 13:45:10 +0000

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - write numbers
+ *
+ * Return: null and his brother void
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+	printf("%d", x);
+	}
+	printf("\n");
+}
