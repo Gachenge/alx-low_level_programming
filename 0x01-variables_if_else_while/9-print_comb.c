55e7c6d0b87d5f96df5e0218e2818a50292bd9f2
Gachenge <gachenge1@gmail.com>
2022-09-09 17:54:49 +0000

    break a loop

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,17 +9,12 @@ int main(void)
 {
 	int num = 0;
 
-	while (num <= 9)
+	for (num = 0; num < 10; num++)
 	{
-		putchar((num % 10) + '0');
-		if (num < 9)
-		{
+		putchar ((num % 10) + '0');
+		continue;
 		putchar(',');
 		putchar(' ');
-		num++;
-		}
-		else
-			return (0);
 	}
 	return (0);
 }
