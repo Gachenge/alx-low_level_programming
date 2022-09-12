a415860d4a5738d731f387450bb2e39f7535d75a
Gachenge <gachenge1@gmail.com>
2022-09-12 07:01:07 +0000

    aphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,9 +7,14 @@
 int main(void)
 {
 	int x;
-	for (x = 0; x < 10; x++)
+
+	for (x = 'a'; x <= 'z'; x++)
 	{
-		putchar(x);
+		if (x == 'e' || x == 'q')
+		{
+			continue;
+		}
+		putchar (x);
 	}
 	putchar('\n');
 	return (0);
