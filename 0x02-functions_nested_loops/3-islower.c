6c4190050e6c291df0684051c540cbf7f444c0ad
Gachenge <gachenge1@gmail.com>
2022-09-13 14:23:52 +0000

    check case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase character
+ *
+ * Return: depends
+ */
+
+int _islower(int c)
+{
+
+	if (c >= 'a' || c <= 'z')
+	{
+		c = 1;
+		else 
+			c = 0;
+	}
+}
