6d4998bcd20216fde56b888cec96b8a7946b0189
Gachenge <gachenge1@gmail.com>
2022-09-13 09:05:23 +0000

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - print the alphabet
+ *
+ * Return: nothing
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return (0);
+}
