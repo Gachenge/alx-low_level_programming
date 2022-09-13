e14c73ae0d6b63041dca88dad56bb569c5e38500
Gachenge <gachenge1@gmail.com>
2022-09-13 14:47:49 +0000

    case sensitive

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,13 +2,15 @@
 
 /**
  * _islower - checks for lowercase character
- * 
+ * c_islower: is it?
+ * int c: entered from main function
+ * Description: 'program'
  * Return: depends
  */
 
 int _islower(int c)
 {
-	if (c >= 'a' || c <= 'z')
+	if (c >= 'a' && c <= 'z')
 		c = 1;
 	else
 		c = 0;
