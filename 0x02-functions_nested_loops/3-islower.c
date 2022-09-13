2af98b54147c278f0cd1dbdae21c98d61d84f1d2
Gachenge <gachenge1@gmail.com>
2022-09-13 14:32:09 +0000

    check case

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,17 +2,14 @@
 
 /**
  * _islower - checks for lowercase character
- *
+ * 
  * Return: depends
  */
 
 int _islower(int c)
 {
-
 	if (c >= 'a' || c <= 'z')
-	{
 		c = 1;
-		else 
-			c = 0;
-	}
+	else
+		c = 0;
 }
