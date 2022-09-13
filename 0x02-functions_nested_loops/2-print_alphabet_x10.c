db66deb44e688f42293b4940e9b72e90eaac0aab
Gachenge <gachenge1@gmail.com>
2022-09-13 13:53:46 +0000

    alphabet ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times
+ *  Return: hakuna
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+	_putchar(i);
+		for (j = 0; j <= 10; j++)
+{
+		_putchar('\n');
+}
+}
+}
+
