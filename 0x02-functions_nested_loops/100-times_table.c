392ea3cc9f5488a90e71a5d1d0c875703666e20c
Gachenge <gachenge1@gmail.com>
2022-09-14 16:00:25 +0000

    n times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * print_times_table - print multiplication
+ *
+ * Return:zilch
+ */
+
+void print_times_table(int n)
+{
+	int i, j, k;
+
+if (n <= 15 && n >= 0)
+{
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			k = i * j;
+			if (k < 10)
+			{
+				_putchar(k);
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(' ');
+			}
+			else if (k >= 10 && k < 100)
+			{
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
+			else if (k >= 100)
+			{
+				_putchar((k / 100) + '0');
+				_putchar(((k / 10) % 10) + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
+}
