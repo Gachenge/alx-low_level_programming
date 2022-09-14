d36086daaa412f8b3cf885f360fbc5f44da2988a
Gachenge <gachenge1@gmail.com>
2022-09-14 11:29:56 +0000

    train for 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,24 +10,49 @@
 
 void print_to_98(int n)
 {
-	if (n <= 98)
+	if (n <= 98 && n >= 0)
 	{
-		while (n <= 98)
+		while (n <= 98 && n < 10)
 		{
-			_putchar(n);
+			_putchar(n + '0');
 			_putchar(',');
 			_putchar(' ');
 			n++;
 		}
+		while (n <= 98 && n > 10)
+		{
+			_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
+			_putchar(',');
+			_putchar(' ');
+			n++;
+		}
+
 	}
 	else if (n > 98)
 	{
 		while (n > 98)
 		{
-			_putchar(n);
+			if (n / 10)
+				_putchar(n / 10);
+			_putchar(n % 10 + '0');
 			_putchar(',');
 			_putchar(' ');
 			n--;
 		}
 	}
+	else if (n < 0)
+	{
+		n = n * -1;
+		while (n < 98)
+		{
+			_putchar('-');
+		_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
+			_putchar(',');
+			_putchar(' ');
+			n++;
+		}
 }
+}
+
