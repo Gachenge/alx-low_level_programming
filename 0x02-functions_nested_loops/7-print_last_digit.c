76af5e9f05fa1dc08c50384772af9d3608438dad
Gachenge <gachenge1@gmail.com>
2022-09-14 05:36:19 +0000

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit - looks for the last digit in a number
+ * @x: input
+ * Return: zilch
+ */
+
+int print_last_digit(int x)
+{
+	int i;
+
+	i = x % 10;
+
+	return (i);
+}
