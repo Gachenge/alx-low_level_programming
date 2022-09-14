a0a7a44fec49341e5755deb71d92d409fe222d9f
Gachenge <gachenge1@gmail.com>
2022-09-14 05:10:54 +0000

    check if positive or negative

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_sign - check if positive or negative
+ *
+ * @n: input assign n
+ *
+ * Return: zilch
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	return (0);
+}
