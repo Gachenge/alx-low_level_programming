91f278f14e30ef9d57caf2c984ef013484d47cf2
Gachenge <gachenge1@gmail.com>
2022-09-15 15:28:27 +0000

    case sensetive

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _isupper - checks for uppercase
+ * @c: input
+ * Return: 1 0r 0
+ */
+
+int _isupper(int c)
+{
+	int x;
+
+	if (c >= 'a' && c <= 'z')
+	{
+		x = 0;
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+		x = 1;
+	}
+	return (x);
+}
