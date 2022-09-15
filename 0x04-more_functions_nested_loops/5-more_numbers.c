69fc31789b147717ebdbddf21b340fed55440d41
Gachenge <gachenge1@gmail.com>
2022-09-15 18:18:32 +0000

    that putchar

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,16 +14,14 @@ void more_numbers(void)
 		for (j = 0; j < 15; j++)
 		{
 			if (j < 10)
-			{
 				x = j;
-			}
-			else
+			else if
 			{
-				x = j / 10;
-				y = j % 10;
+				y = j / 10;
+				x = j % 10;
+				_putchar(y + '0');
 			}
 			_putchar(x + '0');
-			_putchar(y + '0');
 		}
 		_putchar('\n');
 	}
