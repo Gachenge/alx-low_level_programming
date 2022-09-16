18fd707a1fdc981719fbca1cbb3b5c4c5f8594db
Gachenge <gachenge1@gmail.com>
2022-09-16 02:17:56 +0000

    putchar limits

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,19 +10,21 @@ void more_numbers(void)
 	int i, j, x, y;
 
 	for (i = 0; i < 11; i++)
-	{
+{
 		for (j = 0; j < 15; j++)
 		{
 			if (j < 10)
-				x = j;
-			else if
 			{
-				y = j / 10;
-				x = j % 10;
-				_putchar(y + '0');
+			x = j;
 			}
+				else
+				{
+				x = j / 10;
+				y = j % 10;
+				}
 			_putchar(x + '0');
-		}
+			_putchar(y + '0');
+			}
 		_putchar('\n');
-	}
+}
 }
