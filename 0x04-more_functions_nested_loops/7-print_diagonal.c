3055f9c468c24cef48bdf38129e3729a20930d0f
Gachenge <gachenge1@gmail.com>
2022-09-16 03:29:03 +0000

    diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,15 +10,15 @@ void print_diagonal(int n)
 {
 	int i, j;
 
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
 	for (i = 0; i <= n; i++)
 	{
 		for (j = i; j >= 0; j--)
 		{
 		_putchar(' ');
-		}
-		if (n <=0 )
-		{
-			continue;
 		}
 		_putchar('\\');
 		_putchar('\n');
