2df4bcf3daa5328116ffef4f4f7e23c926e35a04
Gachenge <gachenge1@gmail.com>
2022-09-16 09:44:30 +0000

    long num

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -13,10 +13,9 @@ void print_number(int n)
 		n = n * -1;
 		_putchar('-');
 	}
-	if
+	if (n !=0)
 	{
-		(n / 10);
-		print_number(n / 10);
-	_putchar((n % 10) + '0');
+		print_number(n /10);
+		_putchar((n % 10) + '0');
 	}
 }
