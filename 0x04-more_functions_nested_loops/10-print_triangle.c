e6a40cb9559c1976f5f7280a1a02611c405ecdf7
Gachenge <gachenge1@gmail.com>
2022-09-16 05:32:29 +0000

    bermuda

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,11 +10,14 @@ void print_triangle(int size)
 {
 	int i, j;
 
+	if (size <= 0)
+		_putchar('\n');
+
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size; j++)
 		{
-			if (j < ((size-1) - i))
+			if (j < ((size - 1) - i))
 				_putchar(' ');
 			else
 				_putchar('#');
