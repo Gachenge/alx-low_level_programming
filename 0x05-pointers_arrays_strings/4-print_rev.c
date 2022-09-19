537956610145b01c381c2a199826f046f2a01e94
Gachenge <gachenge1@gmail.com>
2022-09-19 18:13:08 +0000

    reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,12 +10,12 @@ void print_rev(char *s)
 {
 	int i = 0;
 
-	while (*(s+i))
+	while (*(s + i))
 		i++;
-	i = i -1;
+	i = i - 1;
 	while (i >= 0)
 	{
-		_putchar(*(s+i));
+		_putchar(*(s + i));
 		i--;
 	}
 	_putchar('\n');
