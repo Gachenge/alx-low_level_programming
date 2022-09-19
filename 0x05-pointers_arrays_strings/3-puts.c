856991f9cf5bbc8333bf3023211968321e419331
Gachenge <gachenge1@gmail.com>
2022-09-19 12:31:10 +0000

    output the string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - output string
+ * @str: i/o string
+ * Return: null
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+}
