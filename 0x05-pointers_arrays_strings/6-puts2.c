2f4894ae235e1b3ba54e78b4dae6a83bf4d4e97b
Gachenge <gachenge1@gmail.com>
2022-09-20 15:22:25 +0000

    every other

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,13 +8,9 @@
 
 void puts2(char *str)
 {
-	for (str = &str[0]; *str != '\0'; str ++)
+	for (str = &str[0]; *str != '\0'; str +=2)
 {
-	while (*str != '\0')
-	{
 		_putchar(*str);
-	str += 2;
-	}
 }
 	_putchar('\n');
 }
