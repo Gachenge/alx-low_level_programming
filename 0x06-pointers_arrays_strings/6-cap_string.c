6b64beef984bb667f77f4ad13ba4dccf34d76fbd
Gachenge <gachenge1@gmail.com>
2022-09-21 14:57:38 +0000

    capitalise

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalise every first letter
+ * @str: string
+ * Return: always 0
+ */
+
+char *cap_string(char *str)
+{
+	int i;
+
+for (i = 0;  str[i] != '\0'; i++)
+{
+	if (str[i] >= 'a' && str[i] <= 'z')
+	{
+		str[i] = str[i] - 32;
+		continue;
+	}
+	else if (str[i] == ' ')
+	{
+		++i;
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+			continue;
+		}
+	}
+	return (0);
+}
