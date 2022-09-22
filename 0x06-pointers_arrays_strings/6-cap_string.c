320c2603aebb79d985f18260838ed1c01617735d
Gachenge <gachenge1@gmail.com>
2022-09-22 03:57:39 +0000

    first capital

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,12 +14,14 @@ for (i = 0;  str[i] != '\0'; i++)
 {
 	if (str[i] >= 'a' && str[i] <= 'z')
 		str[i] =- 32;
-	else if (str[i] == ' ' || str[i] == '.' || str[i])
+	else if (str[i] == ' ' || str[i] == '.')
 	{
 		i++;
-		if (str[i] >= 'a' && str[i] <=z)
+		if (str[i] >= 'a' && str[i] <= 'z')
 		{
 			str[i] =- 32;
 		}
 	}
+}
+return (str);
 }
