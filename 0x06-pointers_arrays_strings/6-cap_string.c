b861661555662010884dc0e22202edfc12173500
Gachenge <gachenge1@gmail.com>
2022-09-22 04:28:49 +0000

    first capital

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,18 +10,18 @@ char *cap_string(char *str)
 {
 	int i;
 
-for (i = 0;  str[i] != '\0'; i++)
+while (str[i] == '\0')
 {
-	if (str[i] >= 'a' && str[i] <= 'z')
-		str[i] = str[i] - 32;
-	else if (str[i - 1] == ' ' || str[i - 1] == '.')
+	if (str[i] == 0 ||
+		str[i] == ' ' ||
+		str[i] == '.')
 	{
-		i++;
 		if (str[i] >= 'a' && str[i] <= 'z')
 		{
 			str[i] = str[i] - 32;
 		}
 	}
+	i++;
 }
 return (str);
 }
