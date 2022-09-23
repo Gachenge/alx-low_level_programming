454aac2a00baebd7da6760242e86f4ec58d11e42
Gachenge <gachenge1@gmail.com>
2022-09-23 02:35:13 +0000

    rot 13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,26 +8,14 @@
 
 char *rot13(char *str)
 {
-	char arr1[] = "ABCDEFGHIJKLMabcdefghijklm";
-	/*char arr2[] = "NOPQRSTUVWXYZnopqrstuvwxyz";*/
-	int j, i = 0;
+	int i = 0;
 
-	while ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		for (j = 0; j < 26; j++)
-		{
-			if (str[i] == arr1[j])
-			{
-				str[i] += 13;
-				break;
-			}
-			else
-			{
-				str[i] -= 13;
-				break;
-			}
-		}
-		i++;
+		if (*(str + i) >= 'a' && *(srt + i) < 'n')
+			*(str + i) += 13;
+		else if (*(str + i) >= 'n' && *(str + i) <= 'z')
+			*(str + i) -= 13;
 	}
 	return (str);
 }
