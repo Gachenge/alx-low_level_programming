72b5274c150b8b1600359f5662d3d7fb99482b81
Gachenge <gachenge1@gmail.com>
2022-09-26 17:01:42 +0000

    string check

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strstr - looks for substring
+ * @haystack: where to look
+ * @needle: what to look for
+ * Return: non
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (needle[j] == haystack[i])
+				return (needle);
+		}
+	}
+	if (needle)
+		return(needle);
+	return (0);
+}
