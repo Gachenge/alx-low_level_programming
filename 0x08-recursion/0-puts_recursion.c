0b1ba4ac987cb6a326a072dcb448d1c762de031c
Gachenge <gachenge1@gmail.com>
2022-09-28 04:13:10 +0000

    puts

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s: string to print
+ * Return: 0
+ */
+
+void _puts_recursion(char *s)
+{
+
+	while (*s != '\0')
+	{
+		_putchar(*s);
+		s++;
+	}
+	_putchar('\n');
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+char _putchar(char c);
+void _puts_recursion(char *s);
+
+
+
+
+
+
+#endif
