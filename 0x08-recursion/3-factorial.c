d8e1ac419ed05b6be2540cb7b444d9b222712efe
Gachenge <gachenge1@gmail.com>
2022-09-28 08:34:35 +0000

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * factorial - recursion multiply
+ * @n: integer input
+ * Return: always o
+ */
+
+int factorial(int n)
+{
+	if (n == 0)
+		return (1);
+	else if (n < 0)
+		return (-1);
+	return (n * factorial(n-1));
+}
