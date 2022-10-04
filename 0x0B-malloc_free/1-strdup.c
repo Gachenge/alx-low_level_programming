792835e76777625bd39869d249ac9e51cc5198e9
Gachenge <gachenge1@gmail.com>
2022-10-04 04:34:15 +0000

    strdup function

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - return pointer to a new string
+ * @str: string
+ * Return: zero
+ */
+
+char *_strdup(char *str)
+{
+	char *s;
+	int i, j, length;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		length++;
+	}
+		i++;
+
+	s = malloc(sizeof(char) * length);
+
+	if (s == NULL)
+		return (NULL);
+
+	for (j = 0; j < length; j++)
+		s[j] = str[j];
+
+	return (s);
+}
