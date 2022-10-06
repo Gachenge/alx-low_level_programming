4b81985f2acac9ee2802a7c41ddc708fd2a15936
Gachenge <gachenge1@gmail.com>
2022-10-06 06:53:00 +0000

    check malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * malloc_checked - check for malloc failure
+ * @b: unsigned
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *s;
+
+	s = malloc(b * sizeof(int));
+
+	if (s == NULL)
+		exit (98);
+	return (s);
+}
