30756ceb5507138ce2b2c6eba95a2ba665304fca
Gachenge <gachenge1@gmail.com>
2022-10-06 08:30:19 +0000

    from min to max:

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * array_range - array fromm min
+ * @min: smallest element
+ * @max: largest element
+ * Return: always 0
+ */
+
+int *array_range(int min, int max)
+{
+	int *arr, i;
+
+	if (min > max)
+		return (NULL);
+
+	arr = malloc(sizeof(int) * (max + 1) - min);
+
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = min; i <=max; i++)
+		arr[i] = i;
+	return (arr);
+}
