3b7cf030ad579f360e8f9b0862c0d5786bb32717
Gachenge <gachenge1@gmail.com>
2022-10-06 17:40:05 +0000

    correction for calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,11 +14,11 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	arr = malloc(sizeof(int) * nmemb);
+	arr = malloc(sizeof(int) * nmemb * size);
 
 	if (arr == NULL)
 		return (NULL);
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size * nmemb; i++)
 		arr[i] = nmemb;
 	return (arr);
 }
