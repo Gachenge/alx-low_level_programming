7ff23473a6e2282172a6a448cb261d102116e5af
Gachenge <gachenge1@gmail.com>
2022-10-11 18:48:09 +0000

    format

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,16 +12,16 @@ void print_dog(struct dog *d)
 	if (d)
 	{
 		if (d->name)
-			printf("%s\n", d->name);
+			printf("Name: %s\n", d->name);
 		else
-			printf("nil");
+			printf("Name: (nil)\n");
 		if (d->age)
-			printf("%f\n", d->age);
+			printf("Age: %f\n", d->age);
 		else
-			printf("nil");
+			printf("Age: (nil)\n");
 		if (d->owner)
-			printf("%s\n", d->owner);
+			printf("Owner: %s\n", d->owner);
 		else
-			printf("nil");
+			printf("Owner: (nil)\n");
 	}
 }
