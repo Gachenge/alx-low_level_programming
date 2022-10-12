db1f8f93a101adaca7aaa0074a33bdce63f8ac57
Gachenge <gachenge1@gmail.com>
2022-10-12 05:25:02 +0000

    create new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,53 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - another gu
+ * @name: bosco
+ * @age: mzae
+ * @owner: kipii
+ * Return: 0
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int i, j, k;
+	dog_t *dog;
+
+	for (i = 0; name[i]; i++)
+		;
+	for (j = 0; owner[j]; j++)
+		;
+	if (name == NULL || owner == NULL)
+		return (NULL);
+
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+	{
+		return (NULL);
+		free(dog);
+	}
+
+	dog->name = malloc(sizeof(char) * i);
+	if (dog->name == NULL)
+	{
+		return (NULL);
+		free(dog);
+	}
+	else
+		for (k = 0; k <i; k++)
+			dog->name[k] = name[k];
+	
+	dog->owner = malloc(sizeof(char) * j);
+	if (dog->owner == NULL)
+	{
+		return (NULL);
+		free(dog);
+	}
+	else
+		for (k = 0; k < j; k++)
+			dog->owner[k] = owner[k];
+	
+	dog->age = age;
+	return (dog);
+}
