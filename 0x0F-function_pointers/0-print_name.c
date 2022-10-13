88299312d312d743bb770151332a23900f2bd4cf
Gachenge <gachenge1@gmail.com>
2022-10-13 05:52:10 +0000

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - print a name
+ * @name: the name
+ * @f: the return function
+ * Return: always
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL)
+		f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,11 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+#include <stdio.h>
+
+int _putchar(char c);
+void print_name(char *name,void (*f)(char *));
+
+
+
+
+#endif /* FUNCTIONS_H*/
