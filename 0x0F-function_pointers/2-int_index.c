9b513273fcf056ff90fc6f28cc0f98e3d82a6d00
Gachenge <gachenge1@gmail.com>
2022-10-13 14:44:24 +0000

    search

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,9 +12,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i, j;
 
-	if (size <= 0)
-		return (-1);
-	if (array && cmp)
+	if (array && cmp && size > 0)
 	{
 		for (i = 0; i < size; i++)
 		{
