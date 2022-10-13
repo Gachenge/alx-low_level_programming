c66cca0556c87ee153f639b0375e4fe7ab69f062
Gachenge <gachenge1@gmail.com>
2022-10-13 12:51:41 +0000

    check null

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,13 +14,15 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (size <= 0)
 		return (-1);
-
-	for (i = 0; i < size; i++)
+	if (array != NULL)
 	{
-		if (cmp(array[i]))
+		for (i = 0; i < size; i++)
 		{
-			return (i);
-			continue;
+			if (cmp(array[i]))
+			{
+				return (i);
+				continue;
+			}
 		}
 	}
 	return (0);
