829eda265f6882fb2dfabfe766694dc232c2fd06
Gachenge <gachenge1@gmail.com>
2022-10-15 05:26:55 +0000

    check for null

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,8 +12,8 @@ int sum_them_all(const unsigned int n, ...)
 	int sum = 0;
 	va_list ptr;
 	va_start(ptr, n);
-if (n == 0)
-	return (0);
+	if (n == 0)
+		return (0);
 	for (i = 0; i < n; i++)
 		sum += va_arg(ptr, int);
 	va_end(ptr);
