1b7701c80df9ad9692c81d61dfb62c9b1e5982a6
Gachenge <gachenge1@gmail.com>
2022-10-15 05:55:58 +0000

    print numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,23 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - print numbers
+ * @separator: between
+ * @n: int
+ * Return: 0
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ptr;
+	unsigned int i;
+
+	va_start(ptr, n);
+
+	for (i = 0; i < n; i++)
+	{
+		if (separator && i != n-1)
+			printf("%d%s", va_arg(ptr, int), separator);
+	}
+	printf("%d\n", va_arg(ptr, int));
+}
