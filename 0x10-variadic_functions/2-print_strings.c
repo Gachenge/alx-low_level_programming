3ad3861c23ac27a5707843dcbe58e2fc8be3f7a8
Gachenge <gachenge1@gmail.com>
2022-10-15 07:56:23 +0000

    print a string

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - lets print 
+ * @separator: in between
+ * @n: how many
+ * Return: 0
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ptr;
+	unsigned int i;
+	char *str;
+
+	va_start(ptr, n);
+
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(ptr, char*);
+
+			if (str)
+				printf("%s",str);
+			else
+				printf("nil");
+			if (separator && i < n - 1)
+				printf("%s", separator);
+	}
+	printf("\n");
+	va_end(ptr);
+}
