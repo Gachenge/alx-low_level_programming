73ec950c2ade5c0e1158a6b006e315f564b32b08
Gachenge <gachenge1@gmail.com>
2022-10-24 17:31:38 +0000

    before main betty

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,12 +2,13 @@
 
 /**
  * print_first - print string before
- * 
+ *
  */
 
-void print_first (void) __attribute__ ((constructor));
+void print_first(void) __attribute__ ((constructor));
 
-void print_first (void)
+void print_first(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n
+			I bore my house upon my back!\n");
 }
