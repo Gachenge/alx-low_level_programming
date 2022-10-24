946495c182221c13becf39c8045e5861e7edf217
Gachenge <gachenge1@gmail.com>
2022-10-24 15:50:49 +0000

    append to the end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -5,6 +5,7 @@
  * @str: the string
  * Return: 0
  */
+
 int _strlen(char *str)
 {
 	int i;
@@ -14,31 +15,42 @@ int _strlen(char *str)
 	return (i);
 }
 
-
 /**
  * add_node_end - add another element at the end
- * @head double pointer
+ * @head: double pointer
  * @str: the string
  * Return: 0
  */
 
-list_t *add_node_end(list_t ** head, const char *str)
+list_t *add_node_end(list_t **head, const char *str)
 {
-	unsigned int i;
-	list_t *new;
+	list_t *new, *current;
 
 	if (!str)
 		return (NULL);
 
 	new = malloc(sizeof(list_t));
+
 	if (!new)
 		return (NULL);
-
-	new->strdup(str);
+	new->str = strdup(str);
 	if (!new->str)
 	{
 		free(new);
 		return (NULL);
 	}
-	new->len = strlen(str);
-	new->next = 
+	new->len = _strlen(new->str);
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+	current = *head;
+
+	while (current->next)
+		current = current->next;
+	current->next = new;
+	return (new);
+}
