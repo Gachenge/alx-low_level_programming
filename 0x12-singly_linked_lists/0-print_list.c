5647a7d5afb4dd00376895637b4ba057435c7718
Gachenge <gachenge1@gmail.com>
2022-10-24 08:24:52 +0000

    print linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_list - print elements in linked list
+ * h - head
+ * Return: 0
+ */
+
+size_t print_list(const list_t *h)
+{
+	const list_t *current = h;
+	int i = 0;
+	while (current)
+	{
+		printf("[%d] %s\n", current->len, current->str);
+		current = current->next;
+		i++;
+	}
+	return (i);
+}
