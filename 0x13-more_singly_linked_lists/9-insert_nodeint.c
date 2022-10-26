21cec392be9955a7468a7fb47321e371bd65b81a
Gachenge <gachenge1@gmail.com>
2022-10-26 20:05:12 +0000

    insert a node

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,52 @@
+#include "lists.h"
+
+/**
+ * listlen - how mny
+ * @node: begin
+ * Return: length
+ */
+
+unsigned int listlen(listint_t *node)
+{
+	int count = 0;
+
+	if (!node)
+		count = 0;
+	while (node)
+	{
+		node = node->next;
+		count++;
+	}
+	return (count);
+}
+
+/**
+ * insert_nodeint_at_index - insert anothwer node
+ * @head: begin
+ * @idx: position
+ * @n: the data
+ * Return: 0
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new, *temp;
+	unsigned int i = 0;
+
+	if (!*head)
+		return (NULL);
+	if (idx > listlen(*head))
+		return (NULL);
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
+	temp = *head;
+	while (i < idx)
+	{
+		temp = temp->next;
+		i++;
+	}
+	new->n = n;
+	new->next = temp->next;
+	temp->next = new;
+	return (new);
+}
