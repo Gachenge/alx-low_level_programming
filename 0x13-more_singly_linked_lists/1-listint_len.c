14b557f5b2c4ce850f92041e325813cf26d27eec
Gachenge <gachenge1@gmail.com>
2022-10-26 05:41:28 +0000

    add node

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len - how many elements
+ * @h: the head
+ * Return: 0
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+	
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
