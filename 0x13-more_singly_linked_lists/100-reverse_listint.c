b9805b65511527e058e5f719d7c4339f0edfbf94
Gachenge <gachenge1@gmail.com>
2022-10-27 16:49:31 +0000

    reverse

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - reverse the list
+ * @head: begin
+ * Return: 0
+ */
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prev = NULL, *nxt = NULL;
+
+	while ((*head) != NULL)
+	{
+		nxt = (*head)->next;
+		(*head)->next = prev;
+		prev = (*head);
+		(*head) = nxt;
+	}
+	*head = prev;
+	return(*head);
+}
