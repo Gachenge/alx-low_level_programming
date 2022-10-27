49d44b39fd907a6591aa5e90237b2d392491ef23
Gachenge <gachenge1@gmail.com>
2022-10-27 05:10:00 +0000

    add condition

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -55,6 +55,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			temp = temp->next;
 			i++;
 		}
+		if (!temp)
+			return (NULL);
 		new->n = n;
 		new->next = temp->next;
 		temp->next = new;
