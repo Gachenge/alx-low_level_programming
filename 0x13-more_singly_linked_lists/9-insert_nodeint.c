4caa64fcadc107414e227d93a915409dee764559
Gachenge <gachenge1@gmail.com>
2022-10-27 04:26:38 +0000

    add condition

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,32 +1,13 @@
 #include "lists.h"
 
 /**
- * listlen - how mny
- * @node: begin
- * Return: length
- */
-
-unsigned int listlen(listint_t *node)
-{
-	int count = 0;
-
-	if (!node)
-		count = 0;
-	while (node)
-	{
-		node = node->next;
-		count++;
-	}
-	return (count);
-}
-
-/**
  * insert_nodeint_at_index - insert anothwer node
  * @head: begin
  * @idx: position
  * @n: the data
  * Return: 0
  */
+
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new, *temp;
@@ -34,21 +15,18 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (!*head || !head)
 		return (NULL);
-	if (idx > listlen(*head))
-		return (NULL);
 	new = malloc(sizeof(listint_t));
 	if (!new)
 		return (NULL);
-	temp = *head;
-
 	if (idx == 0)
 	{
 		new->n = n;
-		new->next = temp->next;
-		temp->next = new;
+		new->next = (*head)->next;
+		(*head)->next = new;
 	}
 	else
 	{
+		temp = *head;
 		while (i < idx)
 		{
 			temp = temp->next;
