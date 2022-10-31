4aa8665e2bab15ad2c6e35371ac84a60acbc6161
Gachenge <gachenge1@gmail.com>
2022-10-31 15:21:29 +0000

    to flip

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * flip_bits - find difference
+ * @n: one number
+ * @m: to the other
+ * Return: 0
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int x, count = 0;
+
+	x = (n ^ m);
+	while (x)
+	{
+		x = x & (x - 1);
+		count++;
+	}
+	return (count);
+}
