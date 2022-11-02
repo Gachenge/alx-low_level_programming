0c243563a1584d2a3e89bdaebfbfad86fc96f917
Gachenge <gachenge1@gmail.com>
2022-11-02 12:27:50 +0000

    betty

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,20 +4,20 @@
  * create_file - create a new file
  * @filename: the file
  * @text_content: write this
- * @Return: 0
+ * Return: 0
  */
 
 int create_file(const char *filename, char *text_content)
 {
 	int i, w, o;
-	
+
 	if (!filename)
 		return (0);
 	if (!text_content)
 		return (0);
 	for (i = 0; text_content[i]; i++)
 		;
-	o = open(filename, O_CREAT | O_RDWR | O_TRUNC |0000);
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC | 0000);
 	w = write(o, text_content, i);
 
 	if (o == -1 || w == -1)
