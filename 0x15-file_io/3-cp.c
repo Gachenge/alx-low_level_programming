ff9c5fe762596144413b3c1e795c9324b4e9e8d1
Gachenge <gachenge1@gmail.com>
2022-11-02 18:03:52 +0000

    error checking

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -33,13 +33,13 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 	file_from = open(argv[1], O_RDONLY);
-	if (!file_from)
+	if (file_from == -1)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	file_to = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
-	if (!file_to)
+	if (file_to == -1)
 	{
 		dprintf(2, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
