cc8227b87bdcee2ae0236ef852354b83f7954745
Gachenge <gachenge1@gmail.com>
2022-11-02 12:43:43 +0000

    more checks

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,9 +12,9 @@ int create_file(const char *filename, char *text_content)
 	int i = 0, w, o;
 
 	if (filename == NULL)
-		return (0);
+		return (-1);
 	if (text_content == NULL)
-		return (0);
+		return (-1);
 	for (i = 0; text_content[i]; i++)
 		;
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC | 0600);
