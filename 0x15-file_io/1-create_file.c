f110422082f3a820c935d5d53b4e313b98b50bf6
Gachenge <gachenge1@gmail.com>
2022-11-02 13:07:46 +0000

    change file permission

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,20 +9,22 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int i, w, o;
+	int i = 0, w, o;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
-	if (text_content != NULL)
+	if (text_content)
 	{
 		for (i = 0; text_content[i]; i++)
 			;
 	}
-	o = open(filename, O_CREAT | O_RDWR | O_TRUNC | 0600);
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(o, text_content, i);
 
 	if (o == -1 || w == -1)
 		return (-1);
+
 	close(o);
+
 	return (1);
 }
