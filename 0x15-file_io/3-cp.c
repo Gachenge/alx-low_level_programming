9ecf5295ed5f6cf3d1f5fa62a14b048c5ae3f53f
Gachenge <gachenge1@gmail.com>
2022-11-03 07:35:48 +0000

    correction

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -17,6 +17,27 @@ void _close(int opn)
 }
 
 /**
+ * _buffer - create a buffer
+ * @buf: the arg
+ * Return: 0
+ */
+
+char *_buffer(char *buf)
+{
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * 1024);
+
+	if (!buffer)
+	{
+		dprintf(2, "Error: Can't write to %s\n", buffer);
+		exit(99);
+	}
+	return (buffer);
+}
+
+
+/**
  * main - entry point
  * @argc: argument count
  * @argv: argument vector
@@ -24,7 +45,7 @@ void _close(int opn)
  */
 int main(int argc, char *argv[])
 {
-	char buff[1024];
+	char *buffer;
 	int file_from, file_to, c, x;
 
 	if (argc != 3)
@@ -32,36 +53,28 @@ int main(int argc, char *argv[])
 		dprintf(2, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
+	buffer = _buffer(argv[2]);
 	file_from = open(argv[1], O_RDONLY);
-	if (file_from == -1)
+	c = read(file_from, buffer, 1024);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	while (c)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	file_to = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
-	if (file_to == -1)
-	{
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
-	while (c >= 1024)
-	{
-		c = read(file_from, buff, 1024);
-		if (c == -1)
+		if (file_from == -1 || c == -1)
 		{
 			dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-			_close(file_from);
-			_close(file_to);
+			free(buffer);
 			exit(98);
 		}
-		x = write(file_to, buff, c);
-		if (x == -1)
+		x = write(file_to, buffer, c);
+		if (file_to == -1 || x == -1)
 		{
 			dprintf(2, "Error: Can't write to %s\n", argv[2]);
+			free(buffer);
 			exit(99);
 		}
 	}
-		_close(file_to);
-		_close(file_from);
+	_close(file_from);
+	_close(file_to);
+	free(buffer);
 	return (0);
 }
