18faae2274855c1ee590feeff8602878ce217329
Gachenge <gachenge1@gmail.com>
2022-11-03 01:25:30 +0000

    string length

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,6 +18,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		for (i = 0; text_content[i]; i++)
 			;
 	}
+	i++;
 	o = open(filename, O_WRONLY, O_APPEND);
 	w = write(o, text_content, i);
 
