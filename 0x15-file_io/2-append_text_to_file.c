a818266ba5a62348a6ec9234dcd30c86db9146eb
Gachenge <gachenge1@gmail.com>
2022-11-03 01:49:00 +0000

    more checks

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,11 +18,9 @@ int append_text_to_file(const char *filename, char *text_content)
 		for (i = 0; text_content[i]; i++)
 			;
 	}
-	o = open(filename, O_WRONLY, O_APPEND);
-	if (i > 1)
-		w = write(o, text_content, i);
-	else
-		w = -1;
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, i);
+	
 	if (o == -1 || w == -1)
 		return (-1);
 	close(o);
