23fec0caaf95ffede629b94c8651ff8803317951
Gachenge <gachenge1@gmail.com>
2022-11-03 07:50:43 +0000

    correction

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -57,7 +57,7 @@ int main(int argc, char *argv[])
 	file_from = open(argv[1], O_RDONLY);
 	c = read(file_from, buffer, 1024);
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	while (c)
+	while (c > 0)
 	{
 		if (file_from == -1 || c == -1)
 		{
