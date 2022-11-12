2f7803a1ab6fc3f6a66f82c71cfcc6573932e6f7
Gachenge <gachenge1@gmail.com>
2022-11-12 13:17:25 +0000

    old corrections

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,42 +1,20 @@
 #include "main.h"
 
 /**
- * str_len - get string length
- * @ptr: pointer
- * Return: nothing
+ * rev_string - reverse a string
+ * @s: the string
  */
-
-int str_len(char *ptr)
-{
-	int i = 0;
-
-	while (*(ptr + 1) != '\0')
-		i++;
-	return (i);
-}
-/**
- * rev_string - reverse the string
- * @s: input
- * Return: nothing
- */
-
 void rev_string(char *s)
 {
-	int len, i;
-	char *start, *end, temp;
+	int i, len, temp;
 
-	len = str_len(s);
-	start = s;
-	end = s;
-
-	for (i = 0; i < len - 1; i++)
-		end++;
+	len = 0;
+	while (s[len])
+		len++;
 	for (i = 0; i < len / 2; i++)
 	{
-		temp = *end;
-		*end = *start;
-		*start = temp;
-		start++;
-		end--;
+		temp = s[i];
+		s[i] = s[len - i -1];
+		s[len - i - 1] = temp;
 	}
 }
--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,9 +8,11 @@
 
 void puts2(char *str)
 {
-	for (str = &str[0]; *str != '\0'; str += 2)
-{
-		_putchar(*str);
-}
+	int i;
+
+	for (i = 0; str[i]; i += 2)
+	{
+		_putchar(str[i]);
+	}
 	_putchar('\n');
 }
