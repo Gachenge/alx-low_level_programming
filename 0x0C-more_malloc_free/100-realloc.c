2ad72e408ec75b26bb75631f9cddcc66438dc212
Gachenge <gachenge1@gmail.com>
2022-11-14 18:44:31 +0000

    remove test files

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -10,13 +10,21 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	unsigned int *arr, i;
+	unsigned int *arr;
+	int diff;
 
-	if (new_size > old_size)
-		old_size = new_size;
-	if (new_size == old_size)
+	if (new_size == 0 && ptr)
+		return (NULL);
+	diff = new_size - old_size;
+
+	if (diff <= 0)
+		return (NULL);
+	else
 	{
-		return (ptr);
-		free(ptr);
+		arr = malloc(sizeof(int) * new_size);
+		if (!arr)
+			return (NULL);
+		return (arr);
 	}
-
+	return (ptr);
+}
