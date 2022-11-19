6046a5182b6bfafec3f9d13f12ceb26ff97ddaac
Gachenge <gachenge1@gmail.com>
2022-11-19 10:21:17 +0000

    calculator

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -19,6 +19,16 @@ int main(int ac, char **av)
 	num1 = atoi(av[1]);
 	op = av[2];
 	num2 = atoi(av[3]);
+	if (get_op_func(op) == NULL || op[1])
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((*op == '/' || *op == '%') && num2 == 0)
+	{
+		printf("Error");
+		exit(100);
+	}
 	printf("%d\n", get_op_func(op)(num1, num2));
 	return (0);
 }
