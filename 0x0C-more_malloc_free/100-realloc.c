f4e253bbb6cafd515142c16717d948148f0cc539
Gachenge <gachenge1@gmail.com>
2022-11-22 08:10:54 +0000

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -10,21 +10,15 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	unsigned int *arr;
-	int diff;
+	int diff, *pnt;
 
-	if (new_size == 0 && ptr)
+	if (new_size == 0 &&ptr)
 		return (NULL);
 	diff = new_size - old_size;
-
 	if (diff <= 0)
 		return (NULL);
-	else
-	{
-		arr = malloc(sizeof(int) * new_size);
-		if (!arr)
-			return (NULL);
-		return (arr);
-	}
-	return (ptr);
+	pnt = malloc(sizeof(int) * new_size);
+	if (!pnt)
+		return (NULL);
+	return (pnt);
 }
