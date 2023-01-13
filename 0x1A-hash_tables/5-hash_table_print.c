3b8c7561b183e16afb1be791b7dad747248baab3
Gachenge <gachenge1@gmail.com>
2023-01-13 09:26:30 +0000

    print hash table

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -7,29 +7,27 @@
 
 void hash_table_print(const hash_table_t *ht)
 {
-	unsigned long int i;
 	hash_node_t *node;
-	int coma = 0;
+	unsigned long int i;
+	int comma = 0;
 
 	if (!ht)
 		return;
 
 	printf("{");
-
 	for (i = 0; i < ht->size; i++)
 	{
 		if (ht->array[i])
 		{
-			if (coma == 1)
+			if (comma == 1)
 				printf(", ");
 			node = ht->array[i];
-
-			while(node)
+			while (node)
 			{
 				printf("'%s': '%s'", node->key, node->value);
 				node = node->next;
 			}
-			coma = 1;
+			comma = 1;
 		}
 	}
 	printf("}\n");
