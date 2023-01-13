7fcda6cccfc0123ac1505a619387fa0ae9a3cfa5
Gachenge <gachenge1@gmail.com>
2023-01-13 10:30:05 +0000

    free

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -7,27 +7,23 @@
 
 void hash_table_delete(hash_table_t *ht)
 {
-	hash_node_t *node, *temp;
+	hash_node_t *temp, *node;
 	unsigned long int i;
-	hash_table_t *head = ht;
 
 	if (!ht)
 		return;
 	for (i = 0; i < ht->size; i++)
 	{
-		if (ht->array[i])
+		node = ht->array[i];
+		while (node)
 		{
-			node = ht->array[i];
-			while (node)
-			{
-				temp = node->next;
-				free(node->key);
-				free(node->value);
-				free(node);
-				node = temp;
-			}
+			temp = node->next;
+			free(node->key);
+			free(node->value);
+			free(node);
+			node = temp;
 		}
 	}
-	free(head->array);
-	free(head);
+	free(ht->array);
+	free(ht);
 }
