59156b0a7e116e5e5b325a2368700c3e2dded481
Gachenge <gachenge1@gmail.com>
2023-01-13 07:51:59 +0000

    retrieve value associated with key

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieve a value asssotiated with a key
+ * @ht: the table
+ * @key: the key to search for
+ * Return: 0
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *node;
+	unsigned long int index;
+
+	if (!ht || !key || !*key)
+		return (NULL);
+	index = key_index((const unsigned char *)key, ht->size);
+	if (index >= ht->size)
+		return (NULL);
+	node = ht->array[index];
+	while (node)
+	{
+		if (strcmp(node->key, key) == 0)
+		{
+			return (node->value);
+			break;
+		}
+		node = node->next;
+	}
+	return (NULL);
+}
